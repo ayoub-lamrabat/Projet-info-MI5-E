424a8d9a5be056ff785523e42cb062ccb891fae3
ayoub-lamrabat <aylamrabat95@gmail.com>
2026-05-23 17:52:55 +0200

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,46 +1,62 @@
 #include <stdio.h>
 #include <string.h>
 #include "utils.h"
-#include "structures.h"
 
-// Efface le terminal proprement
-void effacer_ecran(void) {
+/*
+ * effacer_ecran
+ * Efface tout le contenu visible du terminal et place le curseur
+ * en haut à gauche. Utilisé avant chaque affichage du plateau
+ * pour éviter que les anciens affichages s'accumulent.
+ */
+void effacer_ecran() {
     printf("\033[2J\033[H");
 }
 
-// Vide la mémoire tampon du clavier
-void vider_buffer(void) {
+/*
+ * vider_buffer
+ * Après un scanf(), les caractères restants dans le buffer clavier
+ * (notamment le '\n' final) peuvent perturber la prochaine saisie.
+ * Cette fonction lit et jette tous ces caractères jusqu'à la fin de ligne.
+ */
+void vider_buffer() {
     int c;
-    while ((c = getchar()) != '\n' && c != EOF) {}
+    while ((c = getchar()) != '\n' && c != EOF) {
+    }
 }
 
-// Sécurise la saisie de texte
+/*
+ * lire_nom
+ * Affiche le message d'invite puis lit un nom de joueur dans nom[].
+ */
 void lire_nom(char nom[], char message[]) {
     int valide = 0;
     while (valide == 0) {
         printf("%s", message);
         if (fgets(nom, MAX_NOM, stdin) == NULL) {
-            printf("  Erreur critique lors de la lecture.\n");
+            printf("Erreur de saisie.\n");
             continue;
         }
-        if (nom[strlen(nom) - 1] == '\n') {
+        if (strlen(nom) > 0 && nom[strlen(nom) - 1] == '\n') {
             nom[strlen(nom) - 1] = '\0';
         }
         if (strlen(nom) > 0) {
             valide = 1;
         } else {
-            printf(ROUGE "  Erreur : Le nom ne peut pas être vide !\n" RESET);
+            printf("Erreur : le nom ne peut pas etre vide.\n");
         }
     }
 }
 
-// Sécurise la saisie de nombres entiers
+/*
+ * lire_entier
+ * Affiche le message d'invite et lit un entier compris entre min et max.
+ */
 int lire_entier(char message[], int min, int max) {
     int valeur;
     printf("%s", message);
     while (scanf("%d", &valeur) != 1 || valeur < min || valeur > max) {
         vider_buffer();
-        printf(ROUGE "  Saisie invalide ! Veuillez entrer un nombre entre %d et %d : " RESET, min, max);
+        printf("Valeur invalide, entrez un nombre entre %d et %d : ", min, max);
     }
     vider_buffer();
     return valeur;
