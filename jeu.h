5e05179b4ed523a5e0476bcb7b302bdacc14a869
ella18h <ellakamarhaha@gmail.com>
2026-05-23 16:05:56 +0200

    Create jeu.h

---
+++ jeu.h
@@ -0,0 +1,12 @@
+#ifndef JEU_H
+#define JEU_H
+
+#include "structures.h"
+
+int choisir_arme(void);
+int resoudre_combat(int arme, char monstre);
+void logique_portail(int *joueur_x, int *joueur_y);
+void logique_totem(Case plateau[TAILLE][TAILLE], int *x, int *y, int depx, int depy);
+int verifier_victoire(int *coffre, int *arme_trouvee, char case_actuelle, char arme_recherche);
+
+#endif
