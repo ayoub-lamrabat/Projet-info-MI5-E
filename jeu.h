d3c941a7a2404c0e33b58714105181d18d859b46
ayoub-lamrabat <aylamrabat95@gmail.com>
2026-05-23 17:48:31 +0200

    Update jeu.h

--- jeu.h
+++ jeu.h
@@ -3,10 +3,10 @@
 
 #include "structures.h"
 
-int choisir_arme(void);
-int resoudre_combat(int arme, char monstre);
+int  choisir_arme();
+int  resoudre_combat(int arme, char monstre);
 void logique_portail(int *joueur_x, int *joueur_y);
-void logique_totem(Case plateau[TAILLE][TAILLE], int *x, int *y, int depx, int depy);
-int verifier_victoire(int *coffre, int *arme_trouvee, char case_actuelle, char arme_recherche);
+void logique_totem(Case plateau[5][5], int *x, int *y, int depx, int depy);
+int  verifier_victoire(int *coffre, int *arme_trouvee, char case_actuelle, char arme_recherche);
 
 #endif
