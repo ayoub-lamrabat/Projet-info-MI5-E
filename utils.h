f5a10082aebcec90c1717e278b152870899aa848
ella18h <ellakamarhaha@gmail.com>
2026-05-23 16:03:47 +0200

    Create utils.h

---
+++ utils.h
@@ -0,0 +1,9 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+void effacer_ecran(void);
+void vider_buffer(void);
+void lire_nom(char nom[], char message[]);
+int lire_entier(char message[], int min, int max);
+
+#endif
